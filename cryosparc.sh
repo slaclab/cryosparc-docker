77853f38855b224893d1f8295d5171ebeedd4808
Yee-Ting Li <yee379@gmail.com>
2020-02-04 18:04:14 -0800

    separate master and worker

--- cryosparc.sh
+++ cryosparc.sh
@@ -1,4 +1,4 @@
-#!/bin/bash  -e
+#!/bin/bash  -xe
 
 #echo '================================'
 #ls -lah /gpfs
@@ -9,65 +9,65 @@
 export PATH=${CRYOSPARC_MASTER_DIR}/bin:${CRYOSPARC_WORKER_DIR}/bin:${CRYOSPARC_MASTER_DIR}/deps/anaconda/bin/:$PATH
 export HOME=${USER_HOMEDIR}
 
-###
-# initiate jupyter
-###
-#echo "Running JupyterLab..."
-#jupyter-labhub \
-#     --ip='*' --port=8888 \
-#     --hub-api-url=${JUPYTERHUB_API_URL} \
-#     --notebook-dir=${HOME} 
+export CRYOSPARC_MASTER_HOSTNAME=${CRYOSPARC_MASTER_HOSTNAME:-localhost}
 
-# block
+if [ "${CRYOSPARC_ROLE}" == "master" ]; then
 
-###
-# master initiation
-###
+  ###
+  # master initiation
+  ###
+  echo "Starting cryosparc master..."
 
-cd ${CRYOSPARC_MASTER_DIR}
+  cd ${CRYOSPARC_MASTER_DIR}
+  # modify configuration
+  # ls -lah ${CRYOSPARC_MASTER_DIR}/config.sh
+  sed -i --follow-symlinks 's/^export CRYOSPARC_MASTER_HOSTNAME=.*$/export CRYOSPARC_MASTER_HOSTNAME=${CRYOSPARC_MASTER_HOSTNAME}/g' ${CRYOSPARC_MASTER_DIR}/config.sh
+  #sed -i --follow-symlinks 's/^export CRYOSPARC_MASTER_HOSTNAME=.*$/export CRYOSPARC_MASTER_HOSTNAME=${HOSTNAME}/g' ${CRYOSPARC_MASTER_DIR}/config.sh
+  sed -i --follow-symlinks 's/^export CRYOSPARC_LICENSE_ID=.*$/export CRYOSPARC_LICENSE_ID=${CRYOSPARC_LICENSE_ID}/g' ${CRYOSPARC_MASTER_DIR}/config.sh
+  sed -i --follow-symlinks 's/^export CRYOSPARC_DB_PATH=.*$/export CRYOSPARC_DB_PATH=${CRYOSPARC_DATADIR}\/cryosparc2_database/g' ${CRYOSPARC_MASTER_DIR}/config.sh
+  
+  # envs
+  echo "Starting cryoSPARC in ${CRYOSPARC_MASTER_DIR} with..."
+  SOCK_FILE=$(cryosparcm env | grep CRYOSPARC_SUPERVISOR_SOCK_FILE | sed 's/^.*CRYOSPARC_SUPERVISOR_SOCK_FILE=//' | sed 's/"//g')
+  rm -f "${SOCK_FILE}" || true
+  cryosparcm restart
 
-# modify configuration
-ls -lah ${CRYOSPARC_MASTER_DIR}/config.sh
-sed -i --follow-symlinks 's/^export CRYOSPARC_MASTER_HOSTNAME=.*$/export CRYOSPARC_MASTER_HOSTNAME=localhost/g' ${CRYOSPARC_MASTER_DIR}/config.sh
-#sed -i --follow-symlinks 's/^export CRYOSPARC_MASTER_HOSTNAME=.*$/export CRYOSPARC_MASTER_HOSTNAME=${HOSTNAME}/g' ${CRYOSPARC_MASTER_DIR}/config.sh
-sed -i --follow-symlinks 's/^export CRYOSPARC_LICENSE_ID=.*$/export CRYOSPARC_LICENSE_ID=${CRYOSPARC_LICENSE_ID}/g' ${CRYOSPARC_MASTER_DIR}/config.sh
-sed -i --follow-symlinks 's/^export CRYOSPARC_DB_PATH=.*$/export CRYOSPARC_DB_PATH=${CRYOSPARC_DATADIR}\/cryosparc2_database/g' ${CRYOSPARC_MASTER_DIR}/config.sh
+  # always set the passwrod to license
+  cryosparcm createuser --email $(whoami)@slac.stanford.edu --password "${CRYOSPARC_LICENSE_ID}" --name "User"
+  cryosparcm resetpassword --email $(whoami)@slac.stanford.edu --password "${CRYOSPARC_LICENSE_ID}"
+  
+  # need to restart to get login prompt
+  cryosparcm restart
 
-# envs
-echo "Starting cryoSPARC in ${CRYOSPARC_MASTER_DIR} with..."
-cryosparcm env
+  # remove all existing worker threads
+  /app/cryosparc2_master/bin/cryosparcm cli 'get_scheduler_targets()'  | python -c "import sys, ast, json; print( json.dumps(ast.literal_eval(sys.stdin.readline())) )" | jq '.[].name' | sed 's:"::g' | xargs -n1 -I \{\} /app/cryosparc2_master/bin/cryosparcm cli 'remove_scheduler_target_node("'{}'")'
 
-# start
-cryosparcm restart
+else
 
-# add user
-cryosparcm createuser --email $(whoami)@slac.stanford.edu --password "${CRYOSPARC_LICENSE_ID}" --name "User"
-# always set the passwrod to license
-cryosparcm resetpassword --email $(whoami)@slac.stanford.edu --password "${CRYOSPARC_LICENSE_ID}"
+  ###
+  # worker initiation
+  ###
+  echo "Starting cryosparc worker..."
 
-# need to restart to get login prompt
-cryosparcm restart
+  export TMPDIR=${TMPDIR:-/scratch/$(whoami)}
 
-###
-# worker initiation
-###
-cd ${CRYOSPARC_WORKER_DIR}
-sed -i --follow-symlinks 's/^export CRYOSPARC_LICENSE_ID=.*$/export CRYOSPARC_LICENSE_ID=${CRYOSPARC_LICENSE_ID}/g' ${CRYOSPARC_WORKER_DIR}/config.sh
-sed -i --follow-symlinks 's/^export CRYOSPARC_MASTER_HOSTNAME=.*$/export CRYOSPARC_MASTER_HOSTNAME=${HOSTNAME}/g' ${CRYOSPARC_WORKER_DIR}/config.sh
+  cd ${CRYOSPARC_WORKER_DIR}
+  sed -i --follow-symlinks 's/^export CRYOSPARC_LICENSE_ID=.*$/export CRYOSPARC_LICENSE_ID=${CRYOSPARC_LICENSE_ID}/g' ${CRYOSPARC_WORKER_DIR}/config.sh
+  sed -i --follow-symlinks 's/^export CRYOSPARC_MASTER_HOSTNAME=.*$/export CRYOSPARC_MASTER_HOSTNAME=${CRYOSPARC_MASTER_HOSTNAME}/g' ${CRYOSPARC_WORKER_DIR}/config.sh
+  
+  # start worker
+  mkdir -p $TMPDIR
+  
+  #echo cryosparcw connect --update --worker localhost --master cryosparc-${USER} --ssdpath /scratch/$(whoami)/
+  #cryosparcw connect --update --worker localhost --master cryosparc-${USER} --ssdpath /scratch/$(whoami)/
+  #if [ $? == 1 ]; then
+  /app/cryosparc2_worker/bin/cryosparcw connect --worker localhost --master cryosparc-api-$(whoami) --ssdpath $TMPDIR/
+  #fi
 
-# start worker
-mkdir -p /scratch/$(whoami)
-
-# remove all existing worker threads
-/app/cryosparc2_master/bin/cryosparcm cli 'get_scheduler_targets()'  | python -c "import sys, ast, json; print( json.dumps(ast.literal_eval(sys.stdin.readline())) )" | jq '.[].name' | sed 's:"::g' | xargs -n1 -I \{\} /app/cryosparc2_master/bin/cryosparcm cli 'remove_scheduler_target_node("'{}'")'
-
-#echo cryosparcw connect --update --worker localhost --master cryosparc-${USER} --ssdpath /scratch/$(whoami)/
-#cryosparcw connect --update --worker localhost --master cryosparc-${USER} --ssdpath /scratch/$(whoami)/
-#if [ $? == 1 ]; then
-/app/cryosparc2_worker/bin/cryosparcw connect --worker localhost --master cryosparc-api-$(whoami) --ssdpath /scratch/$(whoami)/
-#fi
-
+fi
+ 
 ###
 # monitor forever
 ###
+echo "done... tailing logs..."
 tail -f ${CRYOSPARC_MASTER_DIR}/run/command_core.log
