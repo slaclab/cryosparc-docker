0f74941fb042986a2bfc2ab0ea2bb478e4f8dc27
Yee-Ting Li <yee379@gmail.com>
2020-02-20 10:07:57 -0800

    slurm scripts

---
+++ slurm/shared/cluster_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+
+## {{ project_dir_abs }}    - absolute path to the project dir
+## {{ job_creator }}        - name of the user that created the job (may contain spaces)
+## {{ cryosparc_username }} - cryosparc username of the user that created the job (usually an email)
+
+#SBATCH -A shared -p shared
+#SBATCH -n {{ num_cpu }}
+#SBATCH -N 1
+#SBATCH --gpus {{ num_gpu }}
+#SBATCH --mem={{ (ram_gb*1000)|int }}MB             
+#SBATCH --job-name cryosparc_{{ project_uid }}_{{ job_uid }}
+#SBATCH -o {{ job_dir_abs }}/job-%j.out
+#SBATCH -e {{ job_dir_abs }}/job-%j.err
+
+LD_PRELOAD=""
+nvidia-smi
+echo "CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES}
+
+export TMPDIR="/scratch/${USER}/cryosparc/"
+#{{ project_uid }}/{{ job_uid }}/"
+echo "TMPDIR="$TMPDIR
+mkdir -p ${TMP_DIR}
+
+# load cryosarc
+source /etc/profile.d/modules.sh
+export MODULEPATH=/afs/slac/package/singularity/modulefiles
+module load cryosparc
+# {{ run_cmd }}
+{{ worker_bin_path }} run {{ run_args }} --ssd "${TMPDIR}" > {{ job_log_path_abs }} 2>&1
+
